6a105d1e56865075166eed3de83a5ca75330fe6e
Hadil24A <hadil.ahmed@hva.nl>
2024-06-17 22:20:32 +0200

    begin laag 1 PE

--- public/main.js
+++ public/main.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-    // Open popup
+//     // Open popup
     
-    document.querySelectorAll('.openPopup').forEach(button => {
-        button.addEventListener('click', () => {
-            let popupId = button.getAttribute('data-popup-id')
-            document.getElementById(popupId).classList.add('active')
-            button.closest('#infographic').document.classList.add('zoom')
-        });
-    });
+//     document.querySelectorAll('.openPopup').forEach(button => {
+//         button.addEventListener('click', () => {
+//             let popupId = button.getAttribute('data-popup-id')
+//             document.getElementById(popupId).classList.add('active')
+//             button.closest('#infographic').document.classList.add('zoom')
+//         });
+//     });
 
-    // Close popup
+//     // Close popup
     
-    document.querySelectorAll('.closePopupButton').forEach(button => {
-        button.addEventListener('click', () => {
-            button.closest('.mobilePopup').classList.remove('active')
-        });
-    });
-});
+//     document.querySelectorAll('.closePopupButton').forEach(button => {
+//         button.addEventListener('click', () => {
+//             button.closest('.mobilePopup').classList.remove('active')
+//         });
+//     });
+// });
